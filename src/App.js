70ec83378d564126e7a2d7c4941b10deb41d0d59
Eurelian <44678944+Eurelian@users.noreply.github.com>
2020-04-28 20:30:43 +0200

    28.04 Added Add New PostIt

--- src/App.js
+++ src/App.js
@@ -5,15 +5,12 @@ import {BacklogBoard} from './BacklogBoard'
 import {ToDoBoard} from './TodoBoard';
 import {InProgressBoard} from './InProgressBoard';
 import {DoneBoard} from './DoneBoard';
-import {PostIt} from './AddPostit'
+// import {PostIt} from './AddPostit'
 
 class App extends React.Component{
   constructor(props){
     super(props);
-    this.state={
-      newItem: [
-      {itemId: "",
-    textValue: "" }],
+    this.state={ itemList: [{id:0, text:"HELLO"}],
 
    display: false,
          
@@ -21,9 +18,20 @@ class App extends React.Component{
   }
   
   handleDisplay(){
-  this.setState({display: !this.state.display})
-   
+  this.setState({display: !this.state.display})   
 }
+ 
+
+  handleAdd(event){
+  event.preventDefault();
+  let text = this.refs.text.value;
+  let id = Math.floor((Math.random()*100)+1);
+  console.log(this.refs.text.value)
+  this.setState({itemList: this.state.itemList.concat({id, text})})
+  this.refs.text.value= "";
+   }
+
+
   render(){
   return ( 
   <>  
@@ -42,11 +50,48 @@ class App extends React.Component{
                         <img src={require('./img/trash.png')} alt="Trash"/>
               </div>
             </div>
-                <BacklogBoard/>
-                <ToDoBoard/>
+                <BacklogBoard text={this.state.itemList}/>
                 <InProgressBoard/>
-                <DoneBoard/>              
-                <PostIt display={this.state.display === true ? "" : "display"} onClick={()=>this.handleDisplay()} />
+                <DoneBoard/> 
+
+
+{/* SUBMIT NEW */}
+            
+             <div className={`show ${this.state.display ? "" : "display"}`}>
+              <div className="content">
+                <button className="show_close" onClick={()=>this.handleDisplay()} id="closeButton">X</button>
+                <div className="moveTo">
+                    <p>Section</p>
+                    <select nameName="moveTo" id="moveTo">
+                        <option value="0">Backlog</option>
+                        <option value="1">To Do</option>
+                        <option value="2">In Progress</option>
+                        <option value="3">Done</option>
+                    </select>
+                </div>
+
+                <img src={require("./img/line.png")}/>
+                <form   onSubmit={(e) => this.handleAdd(e)}>
+                {/* TEXT INPUT */}
+                <textarea className="content" rows="9" cols="45" id="textInput" 
+                    placeholder="Put your task in here!" ref="text"></textarea>
+
+                {/*BUTTONS */}
+                <div class="icon">
+                    <img className="icon" src={require("./img/mark.png")} alt="Move" title="Move"/>
+                    <button onClick={()=>this.handleDisplay()} className="submitbutton" type="submit"><img className="icon" src={require("./img/check.png")} alt="Save"/></button>
+                    <img className="icon"  src={require("./img/delete.png")} alt="Delete" title="Delete"/>
+                </div>
+
+                 
+             </form>         
+             </div>  
+             </div>
+          
+
+
+
+                {/* <PostIt display={this.state.display === true ? "" : "display"} onClick={()=>this.handleDisplay()} buttonTest={()=>this.textButton()} /> */}
   </div>
 
 
